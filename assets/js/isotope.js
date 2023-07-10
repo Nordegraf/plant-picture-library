1c1af831bed19787abac2e2ddfc7766eb2bb900b
Nordegraf <Nordegraf@users.noreply.github.com>
2023-07-10 16:42:49 +0000

    deploy: 6b614460a8dcfb471c54e6b961dc69843ff3edf4

--- assets/js/isotope.js
+++ assets/js/isotope.js
@@ -1,25 +1,47 @@
 var $grid = $('.isogrid')
 
+var activeFilters = [];
+
 $(window).on("ready", function () {
   $grid.css("height", "auto");
   $grid.isotope({
     itemSelector: '.element-item',
     layoutMode: 'fitRows',
-    filter: '*'
+    filter: '*',
+    getSortData: {
+      name: '.plant-name', // text from querySelector
+    },
+    sortBy: 'name',
+
   });
   resize_elems();
 });
 
 $('.filter-btn').on('click', function () {
   var filterValue = $(this).attr('data-filter');
-  console.log(filterValue);
-  $grid.isotope({ filter: filterValue });
+
+  if ($(this).hasClass("clicked")) {
+    activeFilters.splice(activeFilters.indexOf(filterValue), 1);
+    $(this).removeClass("clicked");
+  } else {
+    activeFilters.push(filterValue);
+    $(this).addClass("clicked");
+  }
+
+  $grid.isotope({ filter: activeFilters.join(',') });
+});
+
+$('.reset-btn').on('click', function () {
+  activeFilters = [];
+  $('.filter-btn').removeClass("clicked");
+  $grid.isotope({ filter: '*' });
 });
 
 
 $(window).on("resize", function () {
   resize_elems();
 });
+
 
 function resize_elems() {
   var gridwidth = $grid.width();
