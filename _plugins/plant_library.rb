260a39c5595e816036d97373320df414558f61df
Niklas Conen <niklas.conen@student.kit.edu>
2023-07-10 21:02:53 +0200

    remove redundant code

--- _plugins/plant_library.rb
+++ _plugins/plant_library.rb
@@ -12,16 +12,6 @@ module Plants
         @plants   = plants
       end
 
-      def place_in_layout?
-        if @document == @plants.last
-          Jekyll.logger.info "Plant Picture Library:" , "Emplace"
-          super
-        else
-          false
-        end
-      end
-
-
       def run
         rendered = ""
         for plant in @plants
@@ -51,7 +41,6 @@ module Plants
     end
 
     class PlantPage < Jekyll::Page
-
       def initialize(site, base, dir, name, plants)
         @site = site
         @base = base
@@ -98,9 +87,6 @@ module Plants
     end
 
     class PlantThumbPage < Jekyll::Page
-
-      include Jekyll::Filters
-
       def initialize(site, base, dir, id, plant)
         @site = site
         @base = base
@@ -178,90 +164,6 @@ module Plants
           page = PlantThumbPage.new(site, site.source, '/plants/', i, doc)
           site.pages << page
         end
-
-        filter_classes(site, site.collections['plants'].docs[0])
       end
-
-      def filter_classes(site, doc)
-        classes = {}
-        for doc in site.collections['plants'].docs
-          for cat in site.config["plant_attributes"].keys
-            cl = ""
-            for attr in site.config["plant_attributes"][cat]
-              unless doc.data[attr].nil?
-                # split lists and add each value as a class
-                value = doc.data[attr].gsub(/ /, '_')
-                cl += value + " "
-                classes[attr + ";" + value] = cl
-              end
-            end
-          end
-        end
-        site.data["filter_classes"] = classes
-        Jekyll.logger.info "classes: ", classes
-      end
-
-      def jsonfy_plant_attributes(site, doc)
-        jsonstring = "{"
-        for cat in site.config["plant_attributes"].keys
-          num_attr = site.config["plant_attributes"][cat].length
-          first_attr = site.config["plant_attributes"][cat].first
-          jsonstring += "\"#{first_attr}\": ["
-
-          for attr in site.config["plant_attributes"][cat]
-            if attr == site.config["plant_attributes"][cat].last
-              jsonstring += "\"#{doc.data[attr]}\""
-              for i in 1..num_attr-1
-                jsonstring += "]}"
-              end
-            else
-              jsonstring += "{\"#{doc.data[attr]}\": ["
-            end
-          end
-          jsonstring += "]"
-          unless cat == site.config["plant_attributes"].keys.last
-            jsonstring += ","
-          end
-        end
-        jsonstring += "}"
-
-        doc.data['attr_json'] = jsonstring
-        Jekyll.logger.info "JSON: ", jsonstring
-      end
-
-      def generate_attribute_hierarchy(site)
-        for doc in site.collections['plants'].docs
-          jsonfy_plant_attributes(site, doc)
-        end
-
-        data = {}
-        for doc in site.collections['plants'].docs
-          docdata = JSON.load(doc.data['attr_json'])
-          data = merge_recursively(data, docdata)
-        end
-
-        Jekyll.logger.info "data: ", data
-        site.data["filter_hierarchy"] = data
-      end
-
-      def merge_recursively(a, b)
-        if a.is_a?(Hash) && b.is_a?(Hash)
-          a.merge(b) {|key, a_item, b_item| merge_recursively(a_item, b_item) }
-        elsif a.is_a?(Array) && b.is_a?(Array)
-          newl = a | b
-          # if array content are hashes, merge them
-          if a[0].is_a?(Hash) && b[0].is_a?(Hash)
-            newl = []
-            for i in 0..a.length-1
-              newl << merge_recursively(a[i], b[i])
-            end
-          end
-
-          newl
-        else
-          b
-        end
-      end
-
     end
 end
