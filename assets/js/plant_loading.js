19cc4c510debeff76bd640936c05f41fbc7ae8d8
Nordegraf <Nordegraf@users.noreply.github.com>
2023-07-30 13:41:29 +0000

    deploy: 59b37d23713ff04d391a2fcc07cef56e8a8b6f77

---
+++ assets/js/plant_loading.js
@@ -0,0 +1,70 @@
+var contentDiv = document.getElementById("plant-content");
+var numPerPage = 24;
+var pageNum = 0;
+
+// variables
+$.getJSON("/plant-picture-library/data.json", function(plants) {
+
+  plants.sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  addPage(pageNum, plants);
+
+  // hide load more button if there are no more plants
+  if (plants.length <= numPerPage*(pageNum+1)) {
+    $("#load-more").hide();
+  }
+
+  // add pages when buttons are clicked
+  $('#load-more').on("click", function() {
+    pageNum++;
+    addPage(pageNum, plants);
+
+    if (plants.length <= numPerPage*(pageNum+1)) {
+      $("#load-more").hide();
+    }
+  });
+
+  $(document).trigger("ready");
+});
+
+function addPage(pageNum, plants) {
+  // add page to contentDiv
+  var promises = [];
+  var startIndex = pageNum*numPerPage;
+  var endIndex = (pageNum+1)*numPerPage;
+
+  if (endIndex > plants.length) {
+    endIndex = plants.length - 1;
+  }
+
+  for (var i = startIndex; i < endIndex; i++) {
+    plantID = plants[i].id;
+    promises[i - startIndex] = addPlant(plantID);
+  }
+
+  $.when.apply($, promises).done(function() {
+    $grid.isotope('reloadItems').isotope();
+  });
+}
+
+function addPlants(attr, value, plants) {
+    // search through data for plants with attr
+    // add them and wait for them to be added
+    var promises = [];
+
+    for (var i = 0; i < plants.length; i++) {
+      if (plants[i][attr].includes(value)) {
+        promises.push(addPlant(i));
+      }
+    }
+
+    return promises;
+}
+
+function addPlant(id) {
+  return $.get("/plant-picture-library/plants/"+id+".html", function(data) {
+    $("#plant-content").append(data);
+  }).promise();
+}
