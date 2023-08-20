bdf49f4648dc17f576b9052864afe9db5842eb12
Niklas Conen <niklas.conen@student.kit.edu>
2023-08-20 23:20:08 +0200

    added loading on search

--- assets/js/plant_loading.js
+++ assets/js/plant_loading.js
@@ -3,82 +3,133 @@
 
 var contentDiv = document.getElementById("plant-content");
 var numPerPage = 24;
-var pageNum = 0;
+var numEntries = 0;
+var numLoaded = 0;
+var lastLoaded = 0; // index of last loaded plant
 
 // variables
 $.getJSON("{{ "/data.json" | relative_url }}", function(plants) {
+  numEntries = plants.length;
 
   plants.sort(function(a, b) {
     return a.name.localeCompare(b.name);
   });
 
   // add first page
-  addPage(pageNum, plants);
+  addPage(plants);
 
-  // hide load more button if there are no more plants
-  if (plants.length <= numPerPage*(pageNum+1)) {
-    $("#load-more").hide();
-  }
-
-  $(window).trigger("ready");
-
-  // add pages when buttons are clicked
+  // add pages when load button is clicked
   $('#load-more').on("click", function() {
-    pageNum++;
-    addPage(pageNum, plants);
-
-    if (plants.length <= numPerPage*(pageNum+1)) {
-      $("#load-more").hide();
+    var searchValue = $('#search-field').val().toLowerCase();
+    if (searchValue != "") {
+      addPlants("name", searchValue, plants, numPerPage);
+    } else {
+      addPage(plants);
     }
+
   });
 
   $('.search-btn').on('click', function () {
-    var searchValue = $('#search-field').val();
-    $grid.isotope({ filter: function () { return $(this).find('.plant-name').text().includes(searchValue); } });
+    var searchValue = $('#search-field').val().toLowerCase();
+    var promises = [];
+    var numShown = 0;
+
+    $('#load-more').show();
+    $grid.isotope({ filter: function () { return $(this).find('.plant-name').text().toLowerCase().includes(searchValue); } });
+
+    numShown = numLoaded - $('.element-item:hidden').length;
+    numShown %= numPerPage;
+
+    if (numShown < numPerPage) {
+      promises = addPlants("name", searchValue, plants, numPerPage - numShown);
+    }
+
+    // wait for all promises to be fulfilled
+    Promise.allSettled(promises).then(function() {
+      $(window).trigger('plantupdate');
+    });
+
   });
 });
 
-// search if enter is pressed
-$('#search-field').on('keypress', function (e) {
-  if ($(this).val() != '') {
-    if (e.keyCode == 13) {
-      $('.search-btn').click();
-    }
+$(window).on('plantupdate', function() {
+  $grid.isotope('reloadItems').isotope();
+  resize_elems();
+
+  numLoaded = $('.element-item').length;
+
+  if (numLoaded >= numEntries) {
+    $("#load-more").hide();
   }
 });
 
-function addPage(pageNum, plants) {
+
+function addPage(plants) {
   // add page to contentDiv
   var promises = [];
-  var startIndex = pageNum*numPerPage;
-  var endIndex = (pageNum+1)*numPerPage;
+  var endIndex = 0;
+  // round to next multiple of numPerPage
+  var endIndex = lastLoaded + numPerPage - (numLoaded % numPerPage);
 
   if (endIndex > plants.length) {
-    endIndex = plants.length - 1;
+    endIndex = plants.length;
   }
 
-  for (var i = startIndex; i < endIndex; i++) {
+  for (var i = lastLoaded; i < endIndex; i++) {
+
+    if (is_loaded(plants[i].id)) {
+      if (endIndex < plants.length){
+        endIndex++;
+      }
+      continue;
+    }
+
     plantID = plants[i].id;
-    promises[i - startIndex] = addPlant(plantID);
+    promises[i - lastLoaded] = addPlant(plantID);
   }
+
+  lastLoaded = endIndex;
 
   // wait for all promises to be fulfilled
   Promise.allSettled(promises).then(function() {
-    $grid.isotope('reloadItems').isotope();
-    resize_elems();
+    $(window).trigger('plantupdate');
   });
 }
 
-function addPlants(attr, value, plants) {
-    // search through data for plants with attr
-    // add them and wait for them to be added
+function addPlants(attr, value, plants, amount) {
+    // search through data for plants with attr if not already loaded
     var promises = [];
+    var hide = true;
 
     for (var i = 0; i < plants.length; i++) {
-      if (plants[i][attr].includes(value)) {
-        promises.push(addPlant(i));
+      var plant = plants[i];
+      if (plant[attr].toLowerCase().includes(value) && !$(".element-item").is("#"+plant.id)) {
+        promises.push(addPlant(plant.id));
+      }
+
+      if (promises.length >= amount) {
+        // look up if there is at least one more plant to load
+        for (var j = i + 1; j < plants.length; j++) {
+          var plant = plants[j];
+
+          if (plant[attr].toLowerCase().includes(value)) {
+            hide = false;
+            break;
+          }
+
+        }
+
+        break;
       }
     }
+
+    if (hide) {
+      $("#load-more").hide();
+    }
+
+    Promise.allSettled(promises).then(function() {
+      $(window).trigger('plantupdate');
+    });
 
     return promises;
 }
@@ -88,3 +139,20 @@ function addPlant(id) {
     $("#plant-content").append(data);
   }).promise();
 }
+
+function is_loaded(id) {
+  if ($("#"+id).length == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// search if enter is pressed
+$('#search-field').on('keypress', function (e) {
+  if ($(this).val() != '') {
+    if (e.keyCode == 13) {
+      $('.search-btn').click();
+    }
+  }
+});
